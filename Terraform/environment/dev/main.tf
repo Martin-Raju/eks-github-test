8d9d43c13e99c0024550a7a7c1f4cdffdfbcbbc6
martin Raju <martin.raju313@gmail.com>
2025-08-06 16:31:07 +0530

    updated

--- Terraform/environment/dev/main.tf
+++ Terraform/environment/dev/main.tf
@@ -104,7 +104,7 @@ module "eks" {
 
 # Delay auth provider until EKS exists
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
+  name       = module.eks.cluster_name
   depends_on = [module.eks]
 }
 
