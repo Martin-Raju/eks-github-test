1d6d5705d33804af91707a6a9552cd5a1e169675
Martin-Raju <60430744+Martin-Raju@users.noreply.github.com>
2025-08-06 18:26:06 +0530

    Update main.tf

--- Terraform/environment/dev/main.tf
+++ Terraform/environment/dev/main.tf
@@ -51,7 +51,7 @@ module "eks" {
   source                          = "../../modules/eks"
   cluster_name                    = module.label.id
   cluster_version                 = var.kubernetes_version
-  subnet_ids                      = module.vpc.private_subnets
+  subnet_ids                      = module.vpc.public_subnets
   vpc_id                          = module.vpc.vpc_id
   enable_irsa                     = true
   cluster_endpoint_public_access  = true
