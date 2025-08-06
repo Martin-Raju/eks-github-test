791893d706809aa3cf7bb66162bdfcf10da3003a
Martin-Raju <60430744+Martin-Raju@users.noreply.github.com>
2025-08-06 12:51:14 +0530

    Update main.tf

--- Terraform/environment/dev/main.tf
+++ Terraform/environment/dev/main.tf
@@ -2,6 +2,12 @@ provider "aws" {
   region = var.aws_region
 }
 
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
 data "aws_availability_zones" "available" {}
 data "aws_caller_identity" "current" {}
 locals {
@@ -102,4 +108,28 @@ module "eks" {
       autoscaler   = "arn:aws:iam::aws:policy/AutoScalingFullAccess"
     }
   }
+resource "helm_release" "argo_cd" {
+  name       = "argo-cd"
+  namespace  = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = "5.51.6"  # ✅ Check for the latest at: https://artifacthub.io/packages/helm/argo/argo-cd
+  create_namespace = true
+
+  values = [
+    yamlencode({
+      server = {
+        service = {
+          type = "LoadBalancer"
+          ports = {
+            https = 443
+          }
+        }
+        ingress = {
+          enabled = false
+        }
+      }
+    })
+  ]
+}
 }
