90dd2fb3b5273b1f0c3557e7fb66c929a27b3ba3
Martin-Raju <60430744+Martin-Raju@users.noreply.github.com>
2025-08-06 16:30:23 +0530

    Update main.tf

--- Terraform/environment/dev/main.tf
+++ Terraform/environment/dev/main.tf
@@ -108,24 +108,21 @@ data "aws_eks_cluster_auth" "cluster" {
   depends_on = [module.eks]
 }
 
-# Kubernetes Provider (after EKS)
+# Kubernetes Provider 
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 
-  depends_on = [module.eks]
 }
 
-# Helm Provider (after EKS)
+# Helm Provider 
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.cluster.token
   }
-
-  depends_on = [module.eks]
 }
 
 # Deploy ArgoCD via Helm
