f79e872169b3adb85c24c58370d49f7c90dac37e
Martin-Raju <60430744+Martin-Raju@users.noreply.github.com>
2025-08-06 14:40:42 +0530

    Update main.tf

--- Terraform/environment/dev/main.tf
+++ Terraform/environment/dev/main.tf
@@ -2,10 +2,22 @@ provider "aws" {
   region = var.aws_region
 }
 
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
 provider "helm" {
-  kubernetes = {
-    config_path = "${path.module}/kubeconfig.yaml"
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
 }
 
 data "aws_availability_zones" "available" {}
