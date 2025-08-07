d3c8d1ee33388bab4bf15ffe5eb5f78ca64fe62d
Martin-Raju <60430744+Martin-Raju@users.noreply.github.com>
2025-08-07 13:54:53 +0530

    Update main.tf

--- Terraform/environment/dev/main.tf
+++ Terraform/environment/dev/main.tf
@@ -193,7 +193,7 @@ resource "helm_release" "karpenter" {
   }
 
   set {
-    name  = "serviceAccount.annotations.eks\.amazonaws\.com/role-arn"
+    name  = "serviceAccount.annotations.eks.amazonaws.com/role-arn"
     value = aws_iam_role.karpenter_controller.arn
   }
 
