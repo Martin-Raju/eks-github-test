11f8cda309df8f4934cb304bc1346d7c41745378
Martin-Raju <60430744+Martin-Raju@users.noreply.github.com>
2025-08-07 18:33:31 +0530

    Update main.tf

--- Terraform/environment/dev/main.tf
+++ Terraform/environment/dev/main.tf
@@ -53,7 +53,7 @@ module "vpc" {
 # EKS Module
 module "eks" {
   source       = "terraform-aws-modules/eks/aws"
-  version      = "21.0.0"
+  version      = "20.37.2"
   cluster_name = module.label.id
   subnet_ids   = module.vpc.private_subnets
   vpc_id       = module.vpc.vpc_id
