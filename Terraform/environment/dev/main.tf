244c919f5ff83a80a7ad2e671176fbadd722b2a5
Martin-Raju <60430744+Martin-Raju@users.noreply.github.com>
2025-08-07 09:34:51 +0530

    Update main.tf

--- Terraform/environment/dev/main.tf
+++ Terraform/environment/dev/main.tf
@@ -30,6 +30,7 @@ module "vpc" {
   single_nat_gateway   = true
   enable_dns_hostnames = true
   enable_dns_support   = true
+  map_public_ip_on_launch = true
 
   public_subnet_tags = {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
